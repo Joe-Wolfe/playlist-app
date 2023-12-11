88136eb4d4da443e092540b454d9185b841471b3
Joe Wolfe <JoeWolfe0527@gmail.com>
2023-12-10 21:27:27 -0500

    Finished project

--- models.py
+++ models.py
@@ -8,18 +8,26 @@ db = SQLAlchemy()
 class Playlist(db.Model):
     """Playlist."""
 
-    # ADD THE NECESSARY CODE HERE
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text)
+    songs = db.relationship(
+        'Song', secondary='playlist_song', backref='playlists')
 
 
 class Song(db.Model):
     """Song."""
-
-    # ADD THE NECESSARY CODE HERE
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False)
+    artist = db.Column(db.Text)
 
 
 class PlaylistSong(db.Model):
     """Mapping of a playlist to a song."""
 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlist.id'))
+    song_id = db.Column(db.Integer, db.ForeignKey('song.id'))
     # ADD THE NECESSARY CODE HERE
 
 
